f1c1e176717e1fea410d96419f29c8db50575278
vladimir.vlasenko <v.g.vlasenko@yandex.ru>
2024-04-06 17:38:08 +0300

    fix GenericModel

--- src/main/java/com/example/demo/domain/GenericModel.java
+++ src/main/java/com/example/demo/domain/GenericModel.java
@@ -1,19 +1,11 @@
 package com.example.demo.domain;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 /**
  * Абстрактная сущность со служебной информацией
  */
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @MappedSuperclass
 public abstract class GenericModel {
 
